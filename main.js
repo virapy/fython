860c6584aaf38ba90c1a9a3fade393e3cc85258d
Ali Ebrahimian <ali.ebrahimian.2001.12.16@gmail.com>
2025-01-12 17:13:07 +0330

    fix

--- main.js
+++ main.js
@@ -1,169 +1,152 @@
 const translations = {
-    "if": "اگر",
-    "elif": "اگر دیگر",
-    "else": "دیگر",
-    "for": "برای",
-    "in": "در",
-    "or": "یا",
-    "and": "و",
-    "return": "بازگرداندن",
-    "def": "تعریف",
-    "from": "از",
-    "import": "وارد کردن",
-    "class": "کلاس",
-    "while": "تا زمانی که",
-    "as": "به عنوان",
-    "assert": "تایید",
-    "break": "شکستن",
-    "continue": "ادامه دادن",
-    "del": "حذف",
-    "except": "استثناء",
-    "False": "نادرست",
-    "True": "درست",
-    "finally": "در نهایت",
-    "global": "سراسری",
-    "is": "است",
-    "lambda": "تابع ناشناس",
-    "None": "هیچ",
-    "nonlocal": "غیر محلی",
-    "not": "نه",
-    "try": "تلاش",
-    "with": "با",
-    "yield": "بازده"
-};
-
-const builtinFunctions = {
-    "print": "چاپ",
-    "strip": "حذف فاصله",
-    "split": "جدا کردن",
-    "sum": "جمع",
-    "list": "لیست",
-    "abs": "قدر مطلق",
-    "aiter": "تکرارگر غیر همزمان",
-    "all": "همه",
-    "anext": "بعدی غیر همزمان",
-    "any": "هر یک",
-    "ascii": "متن ساده",
-    "bin": "دودویی",
-    "bool": "بولین",
-    "breakpoint": "نقطه توقف",
-    "bytearray": "آرایه بایت",
-    "bytes": "بایت‌ها",
-    "callable": "قابل فراخوانی",
-    "chr": "کاراکتر",
-    "classmethod": "متد کلاس",
-    "compile": "ترجمه",
-    "complex": "عدد مختلط",
-    "delattr": "حذف ویژگی",
-    "dict": "دیکشنری",
-    "dir": "فهرست",
-    "divmod": "باقی‌مانده تقسیم",
-    "enumerate": "شماره‌گذاری",
-    "eval": "ارزیابی",
-    "exec": "اجرا",
-    "filter": "فیلتر",
-    "float": "شناور",
-    "format": "قالب",
-    "frozenset": "مجموعه منجمد",
-    "getattr": "دریافت ویژگی",
-    "globals": "متغیرهای سراسری",
-    "hasattr": "داشتن ویژگی",
-    "hash": "هش",
-    "help": "راهنما",
-    "hex": "هگزادسیمال",
-    "id": "شناسه",
-    "input": "ورودی",
-    "int": "عدد صحیح",
-    "isinstance": "بررسی نمونه",
-    "issubclass": "بررسی زیرکلاس",
-    "iter": "تکرارگر",
-    "len": "طول",
-    "list": "لیست",
-    "locals": "متغیرهای محلی",
-    "map": "نگاشت",
-    "max": "بیشینه",
-    "memoryview": "نمای حافظه",
-    "min": "کمینه",
-    "next": "بعدی",
-    "object": "شیء",
-    "oct": "هشتادسیمال",
-    "open": "باز کردن",
-    "ord": "کد عددی",
-    "pow": "توان",
-    "property": "ویژگی",
-    "range": "دامنه",
-    "repr": "نمایش رشته‌ای",
-    "reversed": "وارونه",
-    "round": "گرد کردن",
-    "set": "مجموعه",
-    "setattr": "تنظیم ویژگی",
-    "slice": "بُرش",
-    "sorted": "مرتب‌سازی شده",
-    "staticmethod": "متد ایستا",
-    "str": "رشته",
-    "sum": "جمع",
-    "super": "ابر کلاس",
-    "tuple": "تاپل",
-    "type": "نوع",
-    "vars": "متغیرها",
-    "zip": "زیپ",
-    "__import__": "وارد کردن"
-};
-
-const exceptions = {
-    "ArithmeticError": "خطای ریاضی",
-    "AssertionError": "خطای تایید",
-    "AttributeError": "خطای ویژگی",
-    "Exception": "استثناء",
-    "EOFError": "خطای پایان فایل",
-    "FloatingPointError": "خطای نقطه شناور",
-    "GeneratorExit": "خروج مولد",
-    "ImportError": "خطای وارد کردن",
-    "IndentationError": "خطای تورفتگی",
-    "IndexError": "خطای شاخص",
-    "KeyError": "خطای کلید",
-    "KeyboardInterrupt": "قطع کیبورد",
-    "LookupError": "خطای جستجو",
-    "MemoryError": "خطای حافظه",
-    "NameError": "خطای نام",
-    "NotImplementedError": "خطای پیاده‌سازی نشده",
-    "OSError": "خطای سیستم عامل",
-    "OverflowError": "خطای سرریز",
-    "ReferenceError": "خطای ارجاع",
-    "RuntimeError": "خطای زمان اجرا",
-    "StopIteration": "پایان تکرار",
-    "SyntaxError": "خطای نحو",
-    "TabError": "خطای تب",
-    "SystemError": "خطای سیستم",
-    "SystemExit": "خروج سیستم",
-    "TypeError": "خطای نوع",
-    "UnboundLocalError": "خطای متغیر محلی بدون مقداردهی",
-    "UnicodeError": "خطای یونی‌کد",
-    "UnicodeEncodeError": "خطای کدگذاری یونی‌کد",
-    "UnicodeDecodeError": "خطای کدگشایی یونی‌کد",
-    "UnicodeTranslateError": "خطای ترجمه یونی‌کد",
-    "ValueError": "خطای مقدار",
-    "ZeroDivisionError": "خطای تقسیم بر صفر"
+    keywords: {
+        "if": "اگر",
+        "elif": "اگر دیگر",
+        "else": "دیگر",
+        "for": "برای",
+        "in": "در",
+        "or": "یا",
+        "and": "و",
+        "return": "بازگرداندن",
+        "def": "تعریف",
+        "from": "از",
+        "import": "وارد کردن",
+        "class": "کلاس",
+        "while": "تا زمانی که",
+        "as": "به عنوان",
+        "assert": "تایید",
+        "break": "شکستن",
+        "continue": "ادامه دادن",
+        "del": "حذف",
+        "except": "استثناء",
+        "False": "نادرست",
+        "True": "درست",
+        "finally": "در نهایت",
+        "global": "سراسری",
+        "is": "است",
+        "lambda": "تابع ناشناس",
+        "None": "هیچ",
+        "nonlocal": "غیر محلی",
+        "not": "نه",
+        "try": "تلاش",
+        "with": "با",
+        "yield": "بازده",
+    },
+    builtinFunctions: {
+        "print": "چاپ",
+        "strip": "حذف فاصله",
+        "split": "جدا کردن",
+        "sum": "جمع",
+        "list": "لیست",
+        "abs": "قدر مطلق",
+        "all": "همه",
+        "any": "هر یک",
+        "bin": "دودویی",
+        "bool": "بولین",
+        "breakpoint": "نقطه توقف",
+        "bytearray": "آرایه بایت",
+        "bytes": "بایت‌ها",
+        "callable": "قابل فراخوانی",
+        "chr": "کاراکتر",
+        "dict": "دیکشنری",
+        "dir": "فهرست",
+        "enumerate": "شماره‌گذاری",
+        "eval": "ارزیابی",
+        "exec": "اجرا",
+        "filter": "فیلتر",
+        "float": "شناور",
+        "format": "قالب",
+        "globals": "متغیرهای سراسری",
+        "hex": "هگزادسیمال",
+        "id": "شناسه",
+        "input": "ورودی",
+        "int": "عدد صحیح",
+        "len": "طول",
+        "max": "بیشینه",
+        "min": "کمینه",
+        "open": "باز کردن",
+        "pow": "توان",
+        "range": "دامنه",
+        "reversed": "وارونه",
+        "round": "گرد کردن",
+        "sorted": "مرتب‌سازی شده",
+        "str": "رشته",
+        "sum": "جمع",
+        "tuple": "تاپل",
+        "type": "نوع",
+        "vars": "متغیرها",
+        "zip": "زیپ",
+    },
+    exceptions: {
+        "ArithmeticError": "خطای ریاضی",
+        "AttributeError": "خطای ویژگی",
+        "Exception": "استثناء",
+        "IndexError": "خطای شاخص",
+        "KeyError": "خطای کلید",
+        "MemoryError": "خطای حافظه",
+        "NameError": "خطای نام",
+        "TypeError": "خطای نوع",
+        "ValueError": "خطای مقدار",
+        "ZeroDivisionError": "خطای تقسیم بر صفر",
+    }
 };
 
 
 
-// Convert fython code to python
 function translateToPython(code) {
     let pythonCode = code;
-    for (const [fa, en] of Object.entries(translations)) {
-        pythonCode = pythonCode.replaceAll(fa, en);
+
+    // ترجمه کلمات کلیدی
+    for (const [en, fa] of Object.entries(translations.keywords)) {
+        const regex = new RegExp(`\\b${fa}\\b`, "g");
+        pythonCode = pythonCode.replace(regex, en);
     }
+
+    // ترجمه توابع داخلی
+    for (const [en, fa] of Object.entries(translations.builtinFunctions)) {
+        const regex = new RegExp(`\\b${fa}\\b`, "g");
+        pythonCode = pythonCode.replace(regex, en);
+    }
+
+    // ترجمه استثناءها
+    for (const [en, fa] of Object.entries(translations.exceptions)) {
+        const regex = new RegExp(`\\b${fa}\\b`, "g");
+        pythonCode = pythonCode.replace(regex, en);
+    }
+
     return pythonCode;
 }
 
-// Run code
-document.getElementById('run-code').addEventListener('click', () => {
-    const code = document.getElementById('code-editor').value;
-    const pythonCode = translateToPython(code);
-    skupt.configure({ output: (text) => document.getElementById('output').innerText += text});
-    skulpt.run(pythonCode);
-});
+function runCode(persianCode) {
+    const pythonCode = translateToPython(persianCode);
+
+    Skulpt.configure({
+        output: function (text) {
+            const outputDiv = document.getElementById('output');
+            outputDiv.innerText += text;
+        },
+        read: function (x) {
+            if (Skulpt.builtinFiles === undefined || Skulpt.builtinFiles["files"][x] === undefined) {
+                throw `File not found: '${x}'`;
+            }
+            return Skulpt.builtinFiles["files"][x];
+        },
+    });
+
+    Skulpt.misceval.asyncToPromise(() => Skulpt.importMainWithBody("<stdin>", false, pythonCode, true))
+        .catch((err) => {
+            const outputDiv = document.getElementById('output');
+            outputDiv.innerText = `خطا: ${err.toString()}`;
+        });
+}
+
+// // Run code
+// document.getElementById('run-code').addEventListener('click', () => {
+//     const code = document.getElementById('code-editor').value;
+//     const pythonCode = translateToPython(code);
+//     skupt.configure({ output: (text) => document.getElementById('output').innerText += text});
+//     skulpt.run(pythonCode);
+// });
 
 // Save in Localstorage
 const editor = document.getElementById('code-editor');
